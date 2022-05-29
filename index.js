304fc3e3d6907b4e43b330766abc60204e040cfb
Jakob Meier <mail@jakobmeier.ch>
2022-05-29 16:14:40 +0200

    Page wide language selection

--- index.js
+++ index.js
@@ -1,69 +1,142 @@
-window.addEventListener('load', (_event) => {
+const courses = {
+    'de': [
+        {
+            file: 'schulung-allgemein-1806',
+            name: 'Allgemein'
+        },
+        {
+            file: 'schulung-sanitat-bula-mobile-einheiten-1950',
+            name: 'Mobile Einheiten'
+        },
+        {
+            file: 'schulung-sanitat-notfallpraxis-und-sanitatsposten-1921',
+            name: 'Notfallpraxis und Sanitätsposten'
+        },
+    ],
+    'fr': [
+        {
+            file: 'fr-schulung-allgemein-1983',
+            name: 'TODO (Allgemein)'
+        },
+        {
+            file: 'fr-schulung-sanitat-bula-mobile-einheiten-2019',
+            name: 'TODO (Mobile Einheiten)'
+        },
+        {
+            file: 'fr-schulung-sanitat-notfallpraxis-und-sanitatsposten-1985',
+            name: 'TODO (Notfallpraxis und Sanitätsposten)'
+        },
+    ],
+    'it': [
+        {
+            file: 'it-schulung-allgemein-1984',
+            name: 'TODO (Allgemein)'
+        },
+        {
+            file: 'it-schulung-sanitat-bula-mobile-einheiten-1979',
+            name: 'TODO (Mobile Einheiten)'
+        },
+        {
+            file: 'it-schulung-sanitat-notfallpraxis-und-sanitatsposten-2028',
+            name: 'TODO (Notfallpraxis und Sanitätsposten)'
+        },
+    ]
+}
+const langs = ['de', 'de', 'de', 'fr', 'fr', 'fr', 'it', 'it', 'it'];
+const names = ['Allgemein', 'Mobile Einheiten', 'Notfallpraxis und Sanitätsposten', 'Allgemein', 'Mobile Einheiten', 'Notfallpraxis und Sanitätsposten', 'Allgemein', 'Mobile Einheiten', 'Notfallpraxis und Sanitätsposten'];
+const titles = {
+    'de': 'Schulung Sanität Bula',
+    'fr': 'Formation Sanitaire Cafe',
+    'it': 'Formazione Sanità Cafe',
+}
+const back = {
+    'de': 'Zurück zur Übersicht',
+    'fr': 'Retour à la vue d\'ensemble',
+    'it': 'Torna alla panoramica',
+}
 
-    const el = document.getElementById('h5p-container');
+window.addEventListener('load', (_event) => {
+    const lang = getLang();
+    if (lang) {
+        const h1 = document.getElementById('title');
+        document.title = titles[lang];
+        h1.innerText = titles[lang];
+    }
+    const nav = document.getElementById('courses');
+    const course = getCourseFromUrl(lang);
+    if (course !== null) {
+        const el = document.getElementById('h5p-container');
+        new H5PStandalone.H5P(el, getCourseOptions(lang, course));
+        const header = nav.firstElementChild;
+        if (lang) {
+            header.innerHTML = `<h1><a href="./?lang=${lang}">← ${back[lang] || '←'}</a></h1>`;
+            ['de','fr','it'].forEach( l => {
+                const a = document.createElement('a');
+                a.href=`/?lang=${l}&course=${course}`;
+                a.innerText = l.toUpperCase();
+                header.appendChild(a);
+            });
+        } else {
+            header.firstElementChild.outerHTML.innerHTML = `<a href="./">←</a>`;
+        }
+    } else {
+        if (!lang || lang === 'de') {
+            const de = createLangDiv('Deutsch');
+            nav.appendChild(de);
+            courses.de.forEach(
+                (_c, i) => de.appendChild(createCourseLink(i, 'de'))
+            );
+        }
+        if (!lang || lang === 'fr') {
+            const fr = createLangDiv('Français');
+            nav.appendChild(fr);
+            courses.fr.forEach(
+                (_c, i) => fr.appendChild(createCourseLink(i, 'fr'))
+            );
+        }
+        if (!lang || lang === 'it') {
+            const it = createLangDiv('Italiano');
+            nav.appendChild(it);
+            courses.it.forEach(
+                (_c, i) => it.appendChild(createCourseLink(i, 'it'))
+            );
+        }
+    }
+});
+
+function getLang() {
     const searchParams = new URLSearchParams(window.location.search);
-    const courses = [
-        'schulung-allgemein-1806',
-        'schulung-sanitat-bula-mobile-einheiten-1950',
-        'schulung-sanitat-notfallpraxis-und-sanitatsposten-1921',
-        'fr-schulung-allgemein-1983',
-        'fr-schulung-sanitat-bula-mobile-einheiten-2019',
-        'fr-schulung-sanitat-notfallpraxis-und-sanitatsposten-1985',
-        'it-schulung-allgemein-1984',
-        'it-schulung-sanitat-bula-mobile-einheiten-1979',
-        'it-schulung-sanitat-notfallpraxis-und-sanitatsposten-2028'
-    ];
-    const langs = ['de', 'de', 'de', 'fr', 'fr', 'fr', 'it', 'it', 'it'];
-    const names = ['Allgemein','Mobile Einheiten','Notfallpraxis und Sanitätsposten','Allgemein','Mobile Einheiten','Notfallpraxis und Sanitätsposten','Allgemein','Mobile Einheiten','Notfallpraxis und Sanitätsposten'];
-    let loaded = false;
-    if (searchParams.has('schulung')) {
-        const index = Number.parseInt(searchParams.get('schulung'));
-        if (!Number.isNaN(index) && courses.length > index) {
-            const course = courses[index];
-            const options = {
-                h5pJsonPath: `/${course}`,
-                frameJs: '/h5p-standalone-assets/frame.bundle.js',
-                frameCss: '/h5p-standalone-assets/styles/h5p.css',
-            }
-            new H5PStandalone.H5P(el, options);
-            loaded = true;
+    if (searchParams.has('lang')) {
+        const lang = searchParams.get('lang').toLowerCase();
+        if (lang === 'de' || lang === 'fr' || lang === 'it') {
+            return lang;
+        }
+    }
+    return null;
+
+}
+function getCourseFromUrl(lang) {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('course')) {
+        const index = Number.parseInt(searchParams.get('course'));
+        if (!Number.isNaN(index) && courses[lang].length > index) {
+            return index;
         }
         else {
             console.error('Could not load course', index);
         }
     }
-    const nav = document.getElementById('courses');
-    if (loaded) {
-        nav.innerHTML = '<a href="./">Zurück zur Übersicht</a>';
-    } else {
-        const de = createLangDiv('Deutsch');
-        const fr = createLangDiv('Français');
-        const it = createLangDiv('Italiano');
-        nav.appendChild(de);
-        nav.appendChild(fr);
-        nav.appendChild(it);
-        const langDivs = { de, fr, it };
-
-        courses.forEach(
-            (_c, i) => {
-                const div = document.createElement('div');
-                div.classList.add('link-canvas');
-                const block = document.createElement('div');
-                block.classList.add('link-block');
-                div.appendChild(block);
-                const a = document.createElement('a');
-                const la = document.createElement('span');
-                //la.appendChild(document.createTextNode('❱'));
-                la.classList.add('triangle');
-                block.appendChild(la);
-                block.appendChild(a);
-                a.appendChild(document.createTextNode(names[i]));
-                a.href = `/?schulung=${i}`;
-                langDivs[langs[i]].appendChild(div);
-            }
-        );
+    return null;
+}
+function getCourseOptions(lang, index) {
+    const course = courses[lang][index].file;
+    const options = {
+        h5pJsonPath: `/${course}`,
+        frameJs: '/h5p-standalone-assets/frame.bundle.js',
+        frameCss: '/h5p-standalone-assets/styles/h5p.css',
     }
-});
+    return options;
+}
 
 function createLangDiv(text) {
     const div = document.createElement('div');
@@ -71,5 +144,25 @@ function createLangDiv(text) {
     div.classList.add('lang-block');
     deh.innerText = text;
     div.appendChild(deh);
+    return div;
+}
+
+function createCourseLink(i, lang) {
+    const div = document.createElement('div');
+    div.classList.add('link-canvas');
+
+    const block = document.createElement('div');
+    block.classList.add('link-block');
+    div.appendChild(block);
+
+    const la = document.createElement('span');
+    la.classList.add('triangle');
+    block.appendChild(la);
+
+    const a = document.createElement('a');
+    block.appendChild(a);
+    a.appendChild(document.createTextNode(courses[lang][i].name));
+    a.href = `/?course=${i}&lang=${lang}`;
+
     return div;
 }
